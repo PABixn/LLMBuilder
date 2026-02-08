4d345c8d118b3235af10bdb309afa807fba960ed
Paweł Brzuszkiewicz <106952246+PABixn@users.noreply.github.com>
2026-02-08 21:29:35 +0100

    Adding generate function in the model code

--- model/model.py
+++ model/model.py
@@ -56,6 +56,44 @@ class ConfigurableGPT(nn.Module):
         else:
             return logits
 
+    @torch.inference_mode()
+    def generate(self, tokens, max_tokens, temperature=1.0, top_k=50, seed=42, repetition_penalty=1.0):
+        assert isinstance(tokens, list)
+
+        device = self.get_device()
+        rng = None
+
+        if temperature > 0:
+            rng = torch.Generator(device=device)
+            rng.manual_seed(seed)
+
+        ids = torch.tensor([tokens], dtype=torch.long, device=device)
+
+        for _ in range(max_tokens):
+            logits = self.forward(ids)
+            logits = logits[:, -1, :]
+
+            if repetition_penalty != 1.0:
+                prev = ids[0].unique()
+                l = logits[0, prev]
+                logits[0, prev] = torch.where(l > 0, l / repetition_penalty, l * repetition_penalty)
+
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+
+            if temperature > 0:
+                logits = logits / temperature
+                probs = F.softmax(logits, dim=-1)
+                next_ids = torch.multinomial(probs, num_samples=1, generator=rng)
+            else:
+                next_ids = torch.argmax(logits, dim=-1, keepdim=True)
+
+            ids = torch.cat((ids, next_ids), dim=1)
+            token = next_ids.item()
+
+            yield token
+
     def setup_optimizer(self, lr=3e-4, weight_decay=0.01, betas=(0.8, 0.95), eps=1e-10):
         use_fused = next(self.parameters()).device.type == "cuda"
 
